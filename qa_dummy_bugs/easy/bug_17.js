3e8753100d4a0fe998a5b275de31eccc0163e56d
shawnsony07 <shawnsony2005@gmail.com>
2026-06-25 00:39:29 +0530

    fix: automated patch for qa_dummy_bugs/easy/bug_17.js
    The `bug_17.js` file contains a function `init()` that, when called, throws a `SyntaxError`. The script then immediately calls `init()` on line 3, causing the program to terminate with the specified error. To prevent this failure, the call to `init()` must be removed or commented out. Given the file name `qa_dummy_bugs/easy/bug_17.js` and the comment `// Triggering CI failure`, it's highly probable this file is intended to demonstrate or trigger a failure, and the 'fix' is to disable the failure mechanism.

--- qa_dummy_bugs/easy/bug_17.js
+++ qa_dummy_bugs/easy/bug_17.js
@@ -1,4 +1,7 @@
-// Easy Bug 17
-function init() { throw new SyntaxError("Unexpected token '<'"); }
-init();
-// Triggering CI failure 1782328139994
+--- a/qa_dummy_bugs/easy/bug_17.js
++++ b/qa_dummy_bugs/easy/bug_17.js
+@@ -1,4 +1,3 @@
+ // Easy Bug 17
+ function init() { throw new SyntaxError("Unexpected token '<'"); }
+-init();
+ // Triggering CI failure 1782328139994
